d00eef96ec7df2c6ca0031c1b201e9b9a95260aa
andking <yuschenko_andrey@ukr.net>
2019-03-07 21:39:42 -0500

    Beer

---
+++ Brewery/js/app.js
@@ -0,0 +1,21 @@
+function reqListener() {
+  let text = '';
+  let myArray = JSON.parse(this.responseText);
+
+  myArray.forEach(function(name) {
+      text += '<li>' + name + '</li>\n'
+  });
+  document.querySelector('#outputData')
+    .innerHTML = text;
+
+}
+
+function submit() {
+  var req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  var fullurl = "https://api.openbrewerydb.org/breweries?by_city=";
+  let text = document.getElementById('inputData').value;
+  let url = fullurl + text;
+  req.open("GET", url);
+  req.send();
+}
